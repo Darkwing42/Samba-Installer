9a49da388d9bdd48f6174d99bef2e4b34b934980
Jean-Marc Wagner <jeanmarc.wagner@gmx.de>
2016-11-23 16:08:33 +0100

    Neue Ordnung

---
+++ CentOs/centos_abfrage.py
@@ -0,0 +1,14 @@
+import ShareingRE
+
+#Abfrage welche Art von Server installiert werden soll.
+print("Welche Art Samba-Server soll installiert werden?\n")
+eingabe = input("Read-Only, Anonymous(mit anonymen Schreibrechten, Secure Server?\n")
+eingabe = eingabe.lower()
+
+
+if eingabe == "read-only":
+   SharingRE.shareDefinitionReadOnly()
+    
+#elif eingabe == "anonymous":
+
+#elif eingabe == "secure server":
--- main.py
+++ main.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
 
-import smbConfCreater, distroCheck, CentOs
+import smbConfCreater, distroCheck
+from CentOs import SharingRE as SRC
 
 
 distroCheck.osCheck() # Prüft welche Linux Distribution installiert ist.
 
 
 
-smbConfCreater.backupSmbConf() # Erstellt die Globale smb.conf und macht ein Backup von der Vorhandenen.
+#smbConfCreater.backupSmbConf() # Erstellt die Globale smb.conf und macht ein Backup von der Vorhandenen.
 
 
-#Abfrage welche Art von Server installiert werden soll.
-print("Welche Art Samba-Server soll installiert werden?\n")
-eingabe = input("Read-Only, Anonymous(mit anonymen Schreibrechten, Secure Server?\n")
-eingabe = eingabe.lower()
-
-
-if eingabe == "read-only":
-   CentOs.shareDefinitionReadOnly()
-    
-#elif eingabe == "anonymous":
-
-#elif eingabe == "secure server":
